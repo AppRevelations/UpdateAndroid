abdb39b4f454bece3edffd780ad6c8ad18f1afe4
kgarg1995 <kgarg119@gmail.com>
2014-08-11 21:33:46 +0530

    only activity added Tabs are to be added

--- src/com/example/updateandroid/Profile.java
+++ src/com/example/updateandroid/Profile.java
@@ -1,5 +1,17 @@
 package com.example.updateandroid;
 
-public class Profile {
+import android.app.Activity;
+import android.os.Bundle;
 
+
+public class Profile extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	
 }
