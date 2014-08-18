62979ab1e60f0baaa604c8f5c1291f439de65ca2
Lakshay Chhikara <lakshay.chhikara09@gmail.com>
2014-08-18 22:45:45 +0530

    Removed unrequired imports.

--- src/com/apprevelations/updateandroid/ChangeFragment.java
+++ src/com/apprevelations/updateandroid/ChangeFragment.java
@@ -19,8 +19,6 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
