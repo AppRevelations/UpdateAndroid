7ac2368b98b191b54ac6b92cf862115a84a01c69
Himanshu Lakra <himanshu.lakra10@gmail.com>
2014-10-18 16:57:42 +0530

    Final Commit

---
+++ src/com/apprevelations/updateandroid/MainActivityPager.java
@@ -0,0 +1,88 @@
+package com.apprevelations.updateandroid;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.Window;
+import android.widget.Button;
+
+@SuppressLint("NewApi")
+public class MainActivityPager extends FragmentActivity 
+		 {
+
+	private ViewPager viewPager;
+	private TabsPagerAdapter mAdapter;
+	
+	Button bt1,bt2,bt3,bt4;
+	
+
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.mainpager);
+		bt1=(Button) findViewById(R.id.btn1);
+    	bt2=(Button) findViewById(R.id.btn2);
+    	bt3=(Button) findViewById(R.id.btn3);
+    	
+		
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+		viewPager.setAdapter(mAdapter);
+	
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				
+				btnAction(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+	}
+	 private void btnAction(int action){
+	    	switch(action){
+	    	  case 0: 
+	    		  setButton(bt2,4,15,R.drawable.rounded_celll2); 
+	    	      setButton(bt1,4,15,R.drawable.rounded_celll);
+	    	      setButton(bt3,4,15,R.drawable.rounded_celll);
+	    	    
+	    	      break;
+	    	 
+	    	  case 1:  setButton(bt1,4,15,R.drawable.rounded_celll2);
+	    	           setButton(bt2,4,15,R.drawable.rounded_celll); 
+	    	           setButton(bt3,4,15,R.drawable.rounded_celll);
+	    	           
+	    	           break;
+	    	           
+	    	  case 2:  setButton(bt3,4,15,R.drawable.rounded_celll2);
+	                   setButton(bt2,4,15,R.drawable.rounded_celll); 
+	                   setButton(bt1,4,15,R.drawable.rounded_celll);
+	                   
+	                 
+	    	  break;
+	    	}
+	   }
+	 private void setButton(Button btn,int w,int h,int c){
+		    	
+		    	btn.setWidth(w);
+		    	btn.setHeight(h);
+		    	btn.setBackgroundResource(c);
+		    }
+	
+
+}
+
+
---
+++ src/com/apprevelations/updateandroid/TabsPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.apprevelations.updateandroid;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int index) {
+
+		switch (index) {
+		case 0:
+			
+			return new Tab1();
+		case 1:
+			
+			return new Tab2();
+		case 2:
+			
+			return new Tab3();
+		}
+
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+	
+		return 3;
+	}
+
+}
